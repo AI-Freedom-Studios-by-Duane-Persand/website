f31063de55ea4ba31852916b48bb71ba54d0e9d5
Wajid Ali <waali.azmi@gmail.com>
2026-01-16 04:34:01 +0500

    fix: export JwtPayload from auth module and remove .refactored template files from build

--- api/src/auth/index.ts
+++ api/src/auth/index.ts
@@ -1,3 +1,4 @@
 export { CurrentUser } from './current-user.decorator';
 export { RequestWithUser } from './types';
 export { JwtAuthGuard } from './jwt-auth.guard';
+export { JwtPayload } from '../infrastructure/interfaces/jwt-payload.interface';
